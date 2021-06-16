4deddad19f6461ba009e173ca264d7f5402d145e
sachinkenny <61895797+sachinkenny@users.noreply.github.com>
2021-06-16 21:31:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { Component } from "react";
+import { Homepage } from "./homepage.component";
+
+class App extends Component {
+  render() {
+    return <Homepage></Homepage>;
+  }
+}
+
+export default App;
